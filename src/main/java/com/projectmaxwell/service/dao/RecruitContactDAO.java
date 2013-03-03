65bdc080335a81d558bf874ee4d085c2c77fd56b
Eric <galactoise@gmail.com>
2013-03-03 01:19:57 -0800

    Added functionality to retrieve recruit contact history by parameters

---
+++ src/main/java/com/projectmaxwell/service/dao/RecruitContactDAO.java
@@ -0,0 +1,14 @@
+package com.projectmaxwell.service.dao;
+
+import com.projectmaxwell.model.RecruitContact;
+import com.projectmaxwell.model.RecruitContactType;
+
+public interface RecruitContactDAO {
+
+	public RecruitContactType[] getRecruitContactTypes();
+
+	public RecruitContact[] getRecruitContactHistoryByParameters(
+			Integer recruitUserId, Integer recruitContactorUserId,
+			Integer maxResults);
+	
+}
---
+++ src/main/java/com/projectmaxwell/service/resource/RecruitContactResource.java
@@ -0,0 +1,39 @@
+package com.projectmaxwell.service.resource;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+
+import com.projectmaxwell.model.RecruitContact;
+import com.projectmaxwell.model.RecruitContactType;
+import com.projectmaxwell.service.dao.RecruitContactDAO;
+import com.projectmaxwell.service.dao.impl.mysql.RecruitContactDAOImpl;
+
+@Consumes("application/json")
+@Produces("application/json")
+@Path("recruitContact")
+public class RecruitContactResource extends AbstractResource {
+	
+	RecruitContactDAO recruitContactDAO;
+	
+	public RecruitContactResource(){
+		super();
+		recruitContactDAO = new RecruitContactDAOImpl();
+	}
+	
+	@GET
+	@Path("/recruitContactTypes")
+	public RecruitContactType[] getChapters(){
+		return recruitContactDAO.getRecruitContactTypes();
+	}
+	
+	@GET
+	public RecruitContact[] getRecruitContactHistory(@QueryParam("recruitUserId") Integer recruitUserId, 
+			@QueryParam("recruitContactorUserId") Integer recruitContactorUserId,
+			@QueryParam("maxResults") Integer maxResults){
+		
+		return recruitContactDAO.getRecruitContactHistoryByParameters(recruitUserId, recruitContactorUserId, maxResults);
+	}
+}
